120cf2fc913c3343b5c6602f0243471f3dc58bf9
Tanishq-2004 <Tanishqgupta.pro@gmail.com>
2026-06-18 03:02:34 +0530

    updated config.py

--- app/config.py
+++ app/config.py
@@ -4,11 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     groq_api_key: str
-    groq_model: str
+    groq_model: str = "llama-3.3-70b-versatile"
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        extra="ignore"
     )
 
 
